f5e594680c7650d7a38791cd103921991a2a10d2
Erik Erice <erik@redmonster.es>
2015-12-15 01:29:06 +0100

    Added a search bar and a table view. The search puts fake results in the table for now.

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,16 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Erik Sebastian de Erice on 15/12/15.
+//  Copyright © 2015 Erik Sebastián de Erice. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    
+    var name = ""
+    var artistName = ""
+    
+}
